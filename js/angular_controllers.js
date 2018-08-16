70482cc41b8825a2c1d554a4a1d03f14948e343f
DevChrisDev <cj.manuel888@gmail.com>
2018-08-17 01:54:49 +0800

    DONE LOGIN N REGISTER

--- js/angular_controllers.js
+++ js/angular_controllers.js
@@ -1,19 +1,43 @@
 theApp.controller('logInCtrlr', function($scope,$http){
 	$scope.logIn = function(){
 		//parse the data as json
-		var sendData = JSON.stringify({"sent_username" : $scope.angUsername , "sent_password" : $scope.angPassword});
-		var link = "http://localhost/xampp/restAPI/api/Hosts/login_hosts.php";
+		 sendData = JSON.stringify({"sent_username" : $scope.angUsername , "sent_password" : $scope.angPassword});
+		 link = "http://localhost/xampp/restAPI/api/Hosts/login_hosts.php";
 		//post function provided by $http
 		$http.post(link,sendData).then(function(response){
 			if(response.data.message == "Host Login Success."){
-				alert("GAGANA BA");
+				alert("Dito yung function na mag dadala sayo sa home at gagawa ng session");
 			}else{
 				$scope.reply = response.data;
 			}
 		}).catch(function(response) {
-		  alert("Error:" + response.status + " " + response);
+		  	console.log(response);
 		});
 
-	}
+	};
 });
 
+theApp.controller('registerCtrlr', function($scope,$http){
+	$scope.signUp = function(){
+		sendData = JSON.stringify({"fname" : $scope.firstname , "mname" : $scope.middlename , "lname" : $scope.lastname  , "password" : $scope.password1 ,  "username" : $scope.username ,  "confirm_pw" : $scope.password2 });
+
+		link = "http://localhost/xampp/restAPI/api/Hosts/register_hosts.php";
+
+		$http.post(link,sendData).then(function(response){
+			if(response.data.message == "Host Registration Success."){
+				alert("Dito yung function na mag dadala sayo sa home at gagawa ng session");
+			}else{
+				$scope.reply = response.data;
+			}
+		}).catch(function(response){
+			console.log(response);
+		});
+	};
+
+	$scope.checkFields = function(){
+		if($scope.middlename == null || $scope.firstname == null || $scope.lastname == null || $scope.username == null || $scope.password1 == null || $scope.password2 == null)
+			return true
+		else
+			return false
+	};
+});
